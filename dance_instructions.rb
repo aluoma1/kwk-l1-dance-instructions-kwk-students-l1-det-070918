b1b2c1ecb92bac18e774c7504ba5bb67b74af7a5
aluoma1 <aluoma1@emich.edu>
2018-07-11 14:35:45 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,16 @@
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end 
 
-# Code your methods below
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the groud"
+  puts "Finish with a small skip-step backward"
+end 
+base_footwork
+def skip_step(starting_hand)
+  puts "Lower #{staring_hand} to the ground"
+  puts "Bounce #{starting_hand} back up slightly, kicking it a few inches back"
+  puts "Left, Right, Left, and Left"
+end 
+skip_step("left")
